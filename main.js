1dec4d5a8a5113c32a7e173e085bcd05ca02b744
rskhan167 <rs.khan167@gmail.com>
2020-07-10 16:31:52 +0530

    added api to fetch posts && Routing to the app

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-Vue.component('static-posts', {
+/*Vue.component('static-posts', {
 
     template: '#static-posts-template',
 
@@ -29,4 +29,65 @@ Vue.component('static-posts', {
 
 new Vue({
     el: '#app'
+});  */
+
+const baseUrl = "http://jsonplaceholder.typicode.com";
+
+const List = {
+    template: '#list-template',
+    data: () => ({
+        posts: []
+    }),
+    mounted(){
+        this.getPosts();
+    },
+    methods: {
+        getPosts() {
+            axios.get(baseUrl + `/posts`).then(response => {
+                    this.posts = response.data;
+                    console.log(this.posts);
+                }).catch(error => {
+                    console.log(error);
+                })
+        }
+    }
+};
+
+const Post = {
+    template: '#post-template',
+    data: () => ({
+        post: null
+    }),
+    mounted() {
+        this.getPosts();
+    },
+    methods: {
+        getPosts() {
+            var id = this.$route.params.id;
+            axios.get(baseUrl + `/posts/` + id).then(response => {
+                this.post = response.data
+                console.log(this.post);
+            }).catch(error => {
+                console.log(error);
+            })
+        }
+    }
+};
+
+var router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            name: 'homepage',
+            path: '/',
+            component: List
+        }, {
+            name: 'post',
+            path: '/:id',
+            component: Post
+        }
+    ]
 });
+
+var vue = new Vue({router});
+var app = vue.$mount('#app');
